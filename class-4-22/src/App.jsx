61aab9ab43f88b4b43b43849b99f3ba3627cae28
anujkshirsagar <anujkshirsagar31@gmail.com>
2025-10-16 14:40:41 +0530

    updated project files

---
+++ class-4-22/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    
+      <div className="card">
+        hi there
+        
+       </div>
+      
+  )
+}
+
+export default App
