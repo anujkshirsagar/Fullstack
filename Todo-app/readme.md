df80b6213e46b85b0e1854d3e6cc03ea11158a1c
anujkshirsagar <anujkshirsagar31@gmail.com>
2025-10-29 12:06:44 +0530

    Updated

---
+++ Todo-app/readme.md
@@ -0,0 +1,7 @@
+## this project contains a simple Todo application
+It has the following features-
+-Anyone can create a todo
+-anyone can see their existing todos
+-Anyone can mark a todo as done
+//intialize anode project
+//put a package.json
--- class-4-22/src/App.jsx
+++ class-4-22/src/App.jsx
@@ -2,17 +2,23 @@ import { useState } from 'react'
 
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    
-      <div className="card">
-        hi there
-        
-       </div>
-      
+function App() {
+  const[count,setCount]=useState(0);
+  return(
+    <div>
+      <CustomButton count ={count} setCount={setCount}></CustomButton>
+    </div> 
   )
 }
+    
+  function CustomButton(props){
+    function onClickHandler(){
+      props.setCount(props.count+1);
+    }
+    return<button onClick={onClickHandler}>
+      counter{props.count}
+    </button>
+  }
 
 export default App
