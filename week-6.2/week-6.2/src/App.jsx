135bd3977a46220ef3d8d4461048e683a9733127
anujkshirsagar <anujkshirsagar31@gmail.com>
2025-11-11 21:12:57 +0530

    Updated

--- week-6.2/week-6.2/src/App.jsx
+++ week-6.2/week-6.2/src/App.jsx
@@ -1,7 +1,7 @@
-import { useState,useMemo} from "react"
+import {useCallback, useState,useMemo, useEffect} from "react";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+
 import React from 'react';
 //useMemo =it helps to skip the part which we dont want to run every time
 /*
@@ -40,5 +40,93 @@ sum from 1 to{inputValue} is {count}
 }
   */
 
-//useCallback 
-export default App
+//memo
+/*
+function App() {
+  const [count, setCount] = useState(0);
+
+  const inputfunction = useCallback(() => {
+    console.log("hi there");
+  }, []);
+
+  return (
+    <div>
+      <ButtonComponent inputfunction={inputfunction} />
+
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Click me {count}
+      </button>
+    </div>
+  );
+}
+
+const ButtonComponent = memo(({ inputfunction }) => {
+  console.log("child render");
+  return (
+    <div>
+      <button onClick={inputfunction}>button clicked</button>
+    </div>
+  );
+});
+
+export default App;
+*/
+
+//Useeffect-when we give dependencies it runs again if we keep it empty it will run once 
+//if suppose i have taken this example:
+//useEffect(()=>
+ /* {
+    console.log("count runs":{count});
+
+  },[count]); in this example whenever we give dependencies if the dependencie changes(count)it will run again
+  */
+ //it is used after render 
+
+function App()
+{
+  const[exchangeData,setExchangeData]=useState({});
+  const[bankData,setBankData]=useState({});
+  const[updateData,setUpdateData]=useState({})
+  console.log("hi there re-render");
+
+  useEffect(function(){
+    
+      setBankData({
+        returns:100
+      });
+
+  },[])
+  useEffect(()=>{
+
+    setExchangeData({
+      returns:100
+    });
+  
+},[])
+useEffect(()=>{
+setTimeout(()=>{
+  setUpdateData({
+    income:100
+  });
+},5000)
+},[])
+const cryptoReturns=useMemo(()=>{
+ return bankData.returns+exchangeData.returns;
+},[exchangeData,bankData]);
+
+const incomeTax=(cryptoReturns+updateData.income)*0.3;
+return(
+  <div>
+    hi there ,your income tax returns are{incomeTax}
+    </div>
+)
+}
+export default App;
+
+//useMemo:
+//useMemo it is used during rendering if the calculation is complex it doesnt run again and again it keeps the calculation in cache and whenever the 
+//dependencies changes it renders otherwise doesnt
